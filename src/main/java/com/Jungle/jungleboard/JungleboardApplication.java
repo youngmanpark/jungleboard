bfaf7f442119643443a41faac3d4527e4a571125
youngmanpark <py980627@gmail.com>
2024-06-06 15:15:44 +0900

    init commit

---
+++ src/main/java/com/Jungle/jungleboard/JungleboardApplication.java
@@ -0,0 +1,13 @@
+package com.Jungle.jungleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungleboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JungleboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Jungle/jungleboard/JungleboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jungle.jungleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungleboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
