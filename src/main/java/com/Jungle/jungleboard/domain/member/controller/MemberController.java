141a80e3e99cd5a9389e89c7b7fb7a833f945852
youngmanpark <py980627@gmail.com>
2024-06-07 16:20:18 +0900

    feat: MemberController 구현

--- src/main/java/com/Jungle/jungleboard/domain/member/controller/MemberController.java
+++ src/main/java/com/Jungle/jungleboard/domain/member/controller/MemberController.java
@@ -4,28 +4,61 @@ import com.Jungle.jungleboard.domain.member.dto.MemberRequestDto;
 import com.Jungle.jungleboard.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/members")
+@RequestMapping("/api/v1/members")
 public class MemberController {
 
 
     private final MemberService memberService;
 
-    @GetMapping("/hi")
-    public String hi() {
-        System.out.println("hi user");
-        return "hi user";
-    }
-
     @PostMapping("/join")
-    public void createMember(@RequestBody MemberRequestDto.CREATE create) {
+    public ResponseEntity<String> createMember(@RequestBody MemberRequestDto.CREATE create) {
         memberService.createMember(create);
-        System.out.println("회원가입 성공");
+        return ResponseEntity.ok("회원 가입이 완료되었습니다.");
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody MemberRequestDto.LOGIN login) {
+        return ResponseEntity.ok(memberService.login(login));
+    }
+
+    @PutMapping("/update/name/{email}")
+    public ResponseEntity<String> updateMemberName(@PathVariable String email, @RequestBody MemberRequestDto.UPDATE update) {
+        memberService.updateMemberName(email, update);
+        return ResponseEntity.ok("회원 정보가 수정되었습니다.");
+    }
+
+    @PutMapping("/update/password/{email}")
+    public ResponseEntity<String> updateMemberPassword(@PathVariable String email, @RequestBody MemberRequestDto.UPDATE update) {
+        memberService.updateMemberPassword(email, update);
+        return ResponseEntity.ok("회원 정보가 수정되었습니다.");
+    }
+
+    @PutMapping("/update/role/{email}")
+    public ResponseEntity<String> updateMemberRole(@PathVariable String email, @RequestBody MemberRequestDto.UPDATE update) {
+        memberService.updateMemberRole(email, update);
+        return ResponseEntity.ok("회원 정보가 수정되었습니다.");
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteMember(@RequestBody MemberRequestDto.DELETE delete) {
+        memberService.deleteMember(delete);
+        return ResponseEntity.ok("회원 탈퇴가 완료되었습니다.");
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<?> getMemberDetailByEmail(@PathVariable String email) {
+        return ResponseEntity.ok(memberService.getMember(new MemberRequestDto.FIND(email)));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getMemberAll() {
+        return ResponseEntity.ok(memberService.getAllMembers());
+    }
 
 }
